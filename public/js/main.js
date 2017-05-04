825e13bbef81269ec81770528d2f2f8a0393690f
Risha <rishallen@gmail.com>
2017-05-03 23:44:56 -0700

    jquery added in js file. Selectable, draggable, fadeIn and slideUp

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
   $('.hidden').css('visibility','visible').hide().fadeIn(6000);
+
+  $(".greeting").click(function(){
+     $("#sub1").slideUp("slow");
+  });
+
+  $("#sub").draggable({axis:"x"});
+
+  $(function () {
+    $("#selectable").selectable({
+      selected: function (event, ui) {
+        var selected = $("li[class$='ui-selected']").length;
+      }
+    });
+  });
+
 });
