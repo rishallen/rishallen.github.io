302de1fa55f5a30e5abf2ea61cc0a149494e9427
Risha <rishallen@gmail.com>
2017-05-31 14:10:30 -0700

    made changes to jquery functions in main.js. clean code in index.html.

--- public/js/main.js
+++ public/js/main.js
@@ -2,22 +2,61 @@ $(document).ready(function () {
 
   $('.hidden').css('visibility','visible').hide().fadeIn(6000);
 
-  $(".greeting").click(function(){
-     $("#sub1").slideUp("slow");
+  $('.hide').css('visibility','visible').hide();
+  $("#flip").click(function(){
+     $("#panel").slideToggle("slow");
   });
 
-  $("#sub").draggable({axis:"x"});
-
-  $(function() {
-   $("#selectable").selectable();
+  //Work Expereince: Software Development Intern
+  $("#click2").click(function(){
+     $("#panel2").slideToggle("slow");
   });
 
-  $(function() {
-   $("#selectable2").selectable();
+  //Work Expereince: Counselor
+  $("#click3").click(function(){
+     $("#panel3").slideToggle("slow");
   });
 
-  $(function() {
-   $("#selectable-summary").selectable();
+  // Project: Tonalyze
+  $("#click4").click(function(){
+     $("#panel4").slideToggle("slow");
+  });
+
+  // Project: Besty
+  $("#click5").click(function(){
+     $("#panel5").slideToggle("slow");
+  });
+
+  // Project: World Traveler
+  $("#click6").click(function(){
+     $("#panel6").slideToggle("slow");
+  });
+
+  // Education: Floodgate
+  $("#click7").click(function(){
+     $("#panel7").slideToggle("slow");
+  });
+
+  // Education: Floodgate
+  $("#click8").click(function(){
+     $("#panel8").slideToggle("slow");
+  });
+
+  // Education: Floodgate
+  $("#click9").click(function(){
+     $("#panel9").slideToggle("slow");
+  });
+
+  // Education: Floodgate
+  $("#click10").click(function(){
+     $("#panel10").slideToggle("slow");
+  });
+
+  //rotate the words in top header
+  $(".rotate").textrotator({
+  animation: "spin",
+  separator: ",",
+  speed: 3000
   });
 
 });
